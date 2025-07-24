e7d4341422ef8d2c6549a230870f9aba15f02fbb
Murilo Weishaupt <muriloweishaupt@gmail.com>
2025-07-24 12:22:58 -0300

    feita Mensagem de conexão e envio manual de localização

---
+++ server/index.js
@@ -0,0 +1,66 @@
+import { createServer } from 'http';
+import {WebSocketServer} from 'ws';
+import url from 'url';
+import { v4 as uuidv4 } from 'uuid';
+
+const server = createServer();
+const wsServer = new WebSocketServer({ server })
+const PORT = 8000
+
+const connections = {}
+const users = {}
+
+
+const broadcast = () => {
+    Object.keys(connections).forEach(uuid => {
+        const connection = connections[uuid]
+        const message = JSON.stringify(users)
+        connection.send(message)
+    })
+}
+
+const handleMessage = (bytes, uuid) => {
+
+    const message = JSON.parse(bytes.toString())
+    const user = users[uuid];
+    user.state = message
+
+    broadcast()
+
+    console.log(message)
+
+}
+
+const handleClose = uuid => {
+
+}
+
+
+
+
+wsServer.on('connection', (connection, request) => {
+    
+    const { username } = url.parse(request.url, true).query
+    const uuid = uuidv4()
+
+    console.log(username)
+    console.log(uuid)
+
+    connections[uuid] = connection
+
+    users[uuid] = {
+        username,
+        state: { }
+    }
+
+     connection.on("message", message => handleMessage(message, uuid))
+     connection.on("close", close => handleClose(uuid))
+
+})
+
+server.listen(PORT, () => {
+    console.log(`Websocket server is running on port ${PORT}`)
+})
+
+
+
